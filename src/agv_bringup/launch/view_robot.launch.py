c6cff39f6bf01e60ceb41cc6924a327f40518e5b
fylphzy <farhantamsa147@gmail.com>
2026-01-29 18:23:20 +0700

    add: URDF + RViz bringup launch

---
+++ src/agv_bringup/launch/view_robot.launch.py
@@ -0,0 +1,58 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_rviz = LaunchConfiguration('rviz')
+
+    xacro_file = os.path.join(
+        get_package_share_directory('agv_description'),
+        'urdf',
+        'agv.urdf.xacro'
+    )
+
+    rviz_config = os.path.join(
+        get_package_share_directory('agv_description'),
+        'rviz',
+        'view_robot.rviz'
+    )
+
+    # PENTING: ada spasi setelah 'xacro'
+    robot_description = ParameterValue(
+        Command(['xacro ', xacro_file]),
+        value_type=str
+    )
+
+    rsp = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': use_sim_time
+        }]
+    )
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config],
+        parameters=[{'use_sim_time': use_sim_time}],
+        condition=IfCondition(use_rviz)
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
+        DeclareLaunchArgument('rviz', default_value='true'),
+        rsp,
+        rviz
+    ])
--- src/agv_bringup/setup.py
+++ src/agv_bringup/setup.py
@@ -1,29 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'agv_bringup'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='wein',
     maintainer_email='farhantamsa147@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    description='Bringup launch files for AGV.',
+    license='MIT',
+    tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
